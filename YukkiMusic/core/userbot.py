e045266ee76221f8ad6e6964c5ab7f2671ba6607
OTANCABUL <119916934+OTANCABUL@users.noreply.github.com>
2023-01-12 14:34:43 +0800

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -87,8 +87,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("mahadappa")
-                await self.two.join_chat("pantekyks")
+                await self.two.join_chat("gbtnyaOtan")
+                await self.two.join_chat("Nyeghhh")
             except:
                 pass
             assistants.append(2)
